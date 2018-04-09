6771ae0fd436a73366afccd5fc486273516c8972
xiaomoziyi <823346486@qq.com>
2018-04-09 16:46:30 +0800

    test

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app" class="app">
+    <div class="my-table">
+      哈哈哈哈，貌似可以用了
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+.app{
+  background: #fff;
+  position: relative;
+  width: 100%;
+  height:100%;
+  .my-table{
+    margin-left: 0.2rem;
+    margin-right: 0.2rem;
+    padding: 0.2rem;
+    font-size: 0.24rem;
+    // width: 100%;
+    text-align: center;
+    color:#333333;
+    background: #f8f8f8;
+  }
+}
+</style>
